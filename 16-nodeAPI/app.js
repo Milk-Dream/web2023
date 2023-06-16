ab0d6b87235eb6b6b2f7566080801ad715ecde0f
小黑 <2908436596@qq.com>
2023-06-17 00:27:01 +0800

    6/17

--- 16-nodeAPI/app.js
+++ 16-nodeAPI/app.js
@@ -8,10 +8,31 @@ const cors = require("cors")
 app.use(cors())
 //配置解析白哦的数据的中间件，只能解析application/x-www-form-urlencoded
 app.use(express.urlencoded({extended:false}))
+//一定要在路由之前封装res.cc的函数
+app.use((req,res,next)=>{
+    res.cc = function(err, status=1) {
+        res.send({
+            status,
+            message: err instanceof Error ? err.message : err
+        })
+    }
+    next()
+})
 
 //导入并且使用模块
 const userRouter = require("./router/user")
 app.use("/api",userRouter)
+
+//定义错误级别的中间件
+app.use((err,req,res,next) => {
+    //验证失败导致的错误
+    if(err instanceof Joi.ValidationError) {
+        return res.cc(err)
+    }
+
+    //未知错误
+    res.cc(err)
+})
 
 
 app.listen(80, ()=>{
--- 16-nodeAPI/router_handler/user.js
+++ 16-nodeAPI/router_handler/user.js
@@ -1,15 +1,24 @@
 //导入数据库操作模块
 const db = require("../db/index")
+//导入bcryptjs
+const bcrypt = require("bcryptjs")
 //注册新用户的处理函数
 exports.regUser = (req, res)=>{
     //获取客户端提交到服务器的用户信息
     const userinfo = req.body
     //对表单中的数据进行合法性校验
     if(!userinfo.username || !userinfo.password) {
-        res.send({
+        /*res.send({
             status:1,
             message:"用户名或密码不合法!"
-        })
+        })*/
+        
+        
+        
+        
+        
+        
+        return res.cc("用户名或密码不合法")
     }
 
 
@@ -18,13 +27,42 @@ exports.regUser = (req, res)=>{
     db.query(sqlStr, userinfo.username, (err, results)=>{
         //执行SQL语句失败
         if(err) {
-            return res.send({status:1,message:err.message})
+            //return res.send({status:1,message:err.message})
+            return res.cc(err)
         }
         //判断用户名是否占用
         if(results.length > 0) {
-            return res.send({status:1,message:"用户名被占用,请更换其他用户名"})
+            //return res.send({status:1,message:"用户名被占用,请更换其他用户名"})
+            return res.cc("用户名被占用，请更换其他用户名")
         }
-        //TODO 用户名可用,后续操作
+        //调用bcrypt.hashSync()对密码进行加密
+        userinfo.password = bcrypt.hashSync(userinfo.password, 10)
+
+        const sql = "insert into ev_users set ?"
+        //调用db.query()执行sql语句
+        db.query(sql,{username:userinfo.username,password:userinfo.password},(err, results)=>{
+            if(err) {
+                /*return res.send({
+                    status: 1,
+                    message: err.message
+                })*/
+                return res.cc(err)
+            }
+            if(err.affectedRows != 1) {
+                /*return res.send({
+                    status:1,
+                    message:"注册用户失败，请稍后再试!"
+                })*/
+
+                return res.cc("注册用户失败，请稍后再试!")
+            }
+            //注册成功
+            /*res.send({
+                status:0,
+                message:"注册成功"
+            })*/
+            res.cc("注册成功!",0)
+        })
     })
     res.send("reguser ok")
 }
