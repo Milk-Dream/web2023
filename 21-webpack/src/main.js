d4285da8c3eec54ca4864373afd189c3d249dcd3
小黑 <2908436596@qq.com>
2023-08-11 00:39:47 +0800

    8/11

--- 21-webpack/src/main.js
+++ 21-webpack/src/main.js
@@ -5,3 +5,24 @@ $("#myUL > li:even").css("color", "blue")
 
 //引入css文件
 import "./css/index.css"
+//引入less文件
+import "./less/index.less"
+
+//手动引入一个图片文件
+//webpack里面万物皆模块
+import imgObj from "./assets/bg1.png"
+
+let theImg = document.createElement("img")
+theImg.src = imgObj;
+document.body.appendChild(theImg)
+
+//8.引入字体图标的样式
+import "./assets/fonts/iconfont.css"
+
+let theI = document.createElement("i")
+theI.className = "iconfont icon-qq"
+document.body.appendChild(theI)
+
+//书写高版本的js语法
+const fn = ()=> {console.log("我是一个箭头函数1")}
+console.log(fn);
--- 21-webpack/webpack.config.js
+++ 21-webpack/webpack.config.js
@@ -10,5 +10,50 @@ module.exports = {
     },
     plugins:[new HtmlWebpackPlugin({
         template:"./public/index.html"//告诉webpack使用插件时候，以我们自己的html文件作为模板去
-    })]
+    })],
+    module: {//加载器配置
+        rules:[//规则
+            {//一个具体的规则对象
+                test:/\.css$/i,//匹配.css结尾的文件
+                use:["style-loader","css-loader"]//让webpack使用这两个loader处理css文件
+                //从右到左的,所以不能颠倒顺序
+                //css-loader:webpack解析css文件-把css代码一起打包进js中
+                //style-loader:css代码插入dom中
+
+            },
+            {
+                test:/\.less/,
+                use:["style-loader","css-loader","less-loader"]
+            },
+            {
+                //图片文件的配置(仅适用于webpack5版本)
+                test: /\.(gif|png|jpg|jpeg)/,
+                type:"asset"//匹配上面的文件后,webpack
+                //如果你设置的是asset模式
+                //以8kb大小区分图片文件
+                //小于8kb的把图片转换为base64
+                //直接吧图片输出dist目录
+                //
+            },
+            {
+                test:/\.(eot|svg|ttf|woff|woff2)$/,
+                type:"asset/resource", //所有的字体图标文件,都输出到dist下
+                generator: {
+                    filename:"font/[name].[hash:6][ext]"//ext会替换成.eot/.woff
+                }
+
+            },
+            {
+                test:/\.m?js$/,
+                exclude:/(node_modules|bower_components)/,//不去匹配这些文件夹下的文件
+                use: {
+                    loader: "babel-loader",//使用这个loader处理js文件
+                    options: {//加载器选项
+                        presets:["@babel/preset-env"]//预设;@babel/preset-env降级规则-按照这里的规则降级我们的js语法
+
+                    }
+                }
+            }
+        ]
+    }
 }
