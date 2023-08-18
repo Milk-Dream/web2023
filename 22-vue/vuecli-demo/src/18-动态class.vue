7c919df0c1025292e6fb7f2fe0d53d53b60f7d5c
小黑 <2908436596@qq.com>
2023-08-19 00:50:07 +0800

    8/19

---
+++ 22-vue/vuecli-demo/src/18-动态class.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <!--语法1
+        :class="{类名:布尔值}"
+        -->
+        <p :class="{red_str:bool}">动态class</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: '22VueApp',
+
+    data() {
+        return {
+            bool:true
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+    .red_str {
+        color: red;
+    }
+
+</style>
---
+++ 22-vue/vuecli-demo/src/19-动态style.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <!--动态style语法
+        :style="{css属性名: 值
+        }"
+        -->
+        <p :style="{backgroundColor:colorStr}">动态style</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: '22VueApp',
+
+    data() {
+        return {
+            colorStr:"red"
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
